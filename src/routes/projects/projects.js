553f590ba47fbdf409ca5488584af26c0d1146ca
notkirb_ <itnojheals@gmail.com>
2026-08-07 22:12:52 -0600

    fix game links

--- src/routes/projects/projects.js
+++ src/routes/projects/projects.js
@@ -23,19 +23,19 @@ export const games = [
     {
         name: "Praying Mantis UMA: Chapter 1 [NSFW]",
         description: "singleplayer 2d platformer shooter adventure [For audiences ages 16 and up | crude humor, suggestive themes]",
-        link: "https://notkirb.com/praying-mantis/index.html",
+        link: "https://myballs.store/praying-mantis/index.html",
         link_hint: "download now",
     },
     {
         name: "// cybersqr();",
         description: "A simple but difficult game about squares.",
-        link: "https://notkirb.com/cybersqr.html",
+        link: "https://myballs.store/cybersqr.html",
         link_hint: "play in browser",
     },
     {
         name: "Silly Clicker",
         description: "A clicker game with deep lore. (lore not yet created)",
-        link: "https://www.notkirb.com/silly-clicker/",
+        link: "https://myballs.store/silly-clicker/",
         link_hint: "play in browser",
     }
 ]
